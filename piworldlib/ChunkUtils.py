514fc773363e2ead56584dbdff855684c66b5286
Halil Ibrahim Dilsiz <hdilsiz@web.de>
2021-03-21 21:23:00 +0100

    Update ChunkUtils.py

--- piworldlib/ChunkUtils.py
+++ piworldlib/ChunkUtils.py
@@ -31,9 +31,9 @@
 
 class ChunkUtils:
     @staticmethod
-    def new3DArray(xSize: int, zSize: int, ySize: int) -> list:
-        return [[[0 for _ in range(ySize)] for _ in range(zSize)] for _ in range(xSize)]
+    def new3DArray(xSize: int, ySize: int, zSize: int) -> list:
+        return [[[0 for _ in range(xSize)] for _ in range(ySize)] for _ in range(zSize)]
         
     @staticmethod
-    def new2DArray(xSize: int, zSize: int) -> list:
-        return [[0 for _ in range(zSize)] for _ in range(xSize)]
+    def new2DArray(xSize: int, ySize: int, zSize: int) -> list:
+        return [[[0 for _ in range(xSize)] for _ in range(ySize)] for _ in range(zSize)]
